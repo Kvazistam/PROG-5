ad7db836218273bc9afd0cc29d537fb06b676d67
Makoveev Nikita <nikitamakoveev564@gmail.com>
2024-09-17 14:36:30 +0300

    make import from gist and add ConnectionError handling

--- Lab-1/activation_script.py
+++ Lab-1/activation_script.py
@@ -1,33 +1,63 @@
 from importlib.abc import PathEntryFinder
 from importlib.util import spec_from_loader
+from json import load
 import re
+import requests
 import sys
-from urllib.request import urlopen
 from urllib.request import urlopen
 
 class URLFinder(PathEntryFinder):
-    def __init__(self, url, available):
+    def __init__(self, url, available, server):
         self.url = url
         self.available = available
+        self.server = server
         
     def find_spec(self, name, target=None):
         if name in self.available:
-            origin = "{}/{}.py".format(self.url, name)
-            loader = URLLoader()
-            return spec_from_loader(name, loader, origin=origin)
+            if self.server == 'local':
+                origin = "{}/{}.py".format(self.url, name)
+                loader = URLLoader()
+                return spec_from_loader(name, loader, origin=origin)
+            elif self.server == 'gist':
+                origin = self.url
+                loader = URLLoader()
+                return spec_from_loader(name, loader, origin=origin)
         
         else:
             return None
         
 def url_hook(some_str):
-      
+    split_str = some_str.split('/')
+    api_gist = "https://api.github.com/gists/"
+    try:
+        resp = requests.get(some_str)
+        if resp.status_code not in range(200,299):
+            print("can't connect to the server")
+            return None
+    except Exception as e:
+        print("Connection error", type(e))
+        return None
+    except ZeroDivisionError:
+        print('шутка')
+        return None
+    
     if not some_str.startswith(("http", "https")):
         raise ImportError
-    with urlopen(some_str) as page: # requests.get()
-        data = page.read().decode("utf-8")
-    filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data)
-    modnames = {name[:-3] for name in filenames}
-    return URLFinder(some_str, modnames)
+    elif  "gist.github.com" in split_str:
+        id_gist=split_str[-1]
+        resp = requests.get(api_gist+id_gist)
+        json_data = resp.json()
+        files = json_data["files"]
+        filenames = list(files.keys())
+        modnames = {name[:-3] for name in filenames}
+        files_head_member = filenames[0]
+        raw_url = files[files_head_member]["raw_url"]
+        return URLFinder(raw_url, modnames, server= 'gist')
+    else:
+        data = requests.get(some_str)
+        filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data.text)
+        modnames = {name[:-3] for name in filenames}
+        return URLFinder(some_str, modnames, server= 'local')
 
 
 sys.path_hooks.append(url_hook)
@@ -38,7 +68,12 @@ class URLLoader:
         return None
     
     def exec_module(self, module):
-        with urlopen(module.__spec__.origin) as page:
-            source = page.read()
+        data = requests.get(module.__spec__.origin)
+        source = data.text
         code = compile(source, module.__spec__.origin, mode="exec")
         exec(code, module.__dict__)
+
+sys.path.append("http://localhost:8000")
+# sys.path.append("https://gist.github.com/Kvazistam/adf464143e7ca5a7519daa973423eee0")
+import my_module
+my_module.myfoo()
